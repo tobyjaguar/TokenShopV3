6709151ce246117bf94e9baac978203880346c74
tobyjaguar <talgya@gmail.com>
2023-02-05 22:07:47 -0800

    refactor for arbitrum nitro goerli

--- tokenshop/src/App.js
+++ tokenshop/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import {
-  WagmiConfig,
   chain,
+  configureChains,
   createClient,
-  configureChains
+  WagmiConfig
 } from 'wagmi'
 import { arbitrum, arbitrumGoerli } from '@wagmi/chains'
 import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit'
@@ -15,7 +15,8 @@ import MyAppBar from './views/AppBar'
 import Home from './views/Home'
 import Shop from './views/Shop'
 
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
+import { publicProvider } from 'wagmi/providers/public';
 
 // polyfills
 import { Buffer } from 'buffer'
@@ -25,8 +26,52 @@ import './App.css'
 import '@rainbow-me/rainbowkit/styles.css'
 import 'react-toastify/dist/ReactToastify.css';
 
+// const arbitrumRinkeby = {
+//   id: chainId.arbitrumRinkeby,
+//   name: 'Arbitrum Rinkeby',
+//   network: 'arbitrum-rinkeby',
+//   nativeCurrency: {
+//     name: 'Arbitrum Rinkeby Ether',
+//     symbol: 'ARETH',
+//     decimals: 18,
+//   },
+//   rpcUrls: {
+//     alchemy: alchemyRpcUrls.arbitrumRinkeby,
+//     infura: infuraRpcUrls.arbitrumRinkeby,
+//     default: 'https://rinkeby.arbitrum.io/rpc',
+//   },
+//   blockExplorers: {
+//     arbitrum: {
+//       name: 'Arbitrum Explorer',
+//       url: 'https://rinkeby-explorer.arbitrum.io',
+//     },
+//     etherscan: etherscanBlockExplorers.arbitrumRinkeby,
+//     default: etherscanBlockExplorers.arbitrumRinkeby,
+//   },
+//   testnet: true,
+// }
+
+const arbitrumNitroGoerliChain = {
+  id: 421_613,
+  name: 'Arbitrum Nitro Goerli',
+  network: 'arbitrum-goerli',
+  nativeCurrency: {
+    name: 'GoerliETH',
+    symbol: 'ETH',
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: 'https://goerli-rollup.arbitrum.io/rpc',
+  },
+  blockExplorers: {
+    default: 'https://goerli.arbiscan.io',
+    etherscan: 'https://goerli-rollup-explorer.arbitrum.io',
+  },
+  testnet: true,
+};
+
 const { chains, provider } = configureChains(
-  [chain.arbitrum, chain.arbitrumRinkeby],
+  [chain.arbitrum, arbitrumNitroGoerliChain],
   [
     jsonRpcProvider({
       rpc: (chain) => {
@@ -41,21 +86,22 @@ const { connectors } = getDefaultWallets({
   chains
 })
 
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors,
+  provider,
+});
+
+
 // polyfill Buffer for client
 if (!window.Buffer) {
   window.Buffer = Buffer
 }
 
-const client = createClient({
-  autoConnect: true,
-  connectors,
-  provider,
-})
-
 const App = () => {
   return (
     <div className="App">
-      <WagmiConfig client={client}>
+      <WagmiConfig client={wagmiClient}>
         <RainbowKitProvider chains={chains}>
         <ToastContainer position='bottom-right' />
           <MyAppBar />
